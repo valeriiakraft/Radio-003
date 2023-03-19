7c8099f38d77679d9f3c2ce20795679606ae90d5
Valeriia Kraft <airkraft20@gmail.com>
2023-03-19 17:42:27 +0700

    new terms added

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -3,9 +3,8 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
     @Test
-    public void nextRadioStationWhenTheValueAre0() {
-        Radio radio = new Radio();
-        radio.setCurrentRadioStationNumber(0);
+    public void nextRadioStationWithConstructorParametersWhenTheValueAreMinRadioStationNumber() {
+        Radio radio = new Radio(16);
         radio.nextRadioStation();
         int expected = 1;
         int actual = radio.getCurrentRadioStationNumber();
@@ -13,7 +12,26 @@ public class RadioTest {
     }
 
     @Test
-    public void nextRadioStationWhenTheValueAre9() {
+    public void nextRadioStationWithoutConstructorParametersWhenTheValueAreMinRadioStationNumber() {
+        Radio radio = new Radio();
+        radio.nextRadioStation();
+        int expected = 1;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nextRadioStationWithConstructorParametersWhenTheValueAreMaxRadioStationNumber() {
+        Radio radio = new Radio(48);
+        radio.setCurrentRadioStationNumber(47);
+        radio.nextRadioStation();
+        int expected = 0;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nextRadioStationWithoutConstructorParametersWhenTheValueAreMaxRadioStationNumber() {
         Radio radio = new Radio();
         radio.setCurrentRadioStationNumber(9);
         radio.nextRadioStation();
@@ -23,18 +41,38 @@ public class RadioTest {
     }
 
     @Test
-    public void nextRadioStationWhenTheValuesAreGreaterThan0AndLessThan9() {
-        Radio radio = new Radio();
-        radio.setCurrentRadioStationNumber(5);
+    public void nextRadioStationWithConstructorParametersWhenTheValuesAreGreaterThanMinRadioStationNumberAndLessThanMaxRadioStationNumber() {
+        Radio radio = new Radio(15);
+        radio.setCurrentRadioStationNumber(10);
         radio.nextRadioStation();
-        int expected = 6;
+        int expected = 11;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nextRadioStationWithoutConstructorParametersWhenTheValuesAreGreaterThanMinRadioStationNumberAndLessThanMaxRadioStationNumber() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStationNumber(4);
+        radio.nextRadioStation();
+        int expected = 5;
         int actual = radio.getCurrentRadioStationNumber();
         Assertions.assertEquals(expected, actual);
     }
 
 
     @Test
-    public void prevRadioStationWhenTheValueAre0() {
+    public void prevRadioStationWithConstructorParametersWhenTheValueAreMinRadioStationNumber() {
+        Radio radio = new Radio(17);
+        radio.setCurrentRadioStationNumber(0);
+        radio.prevRadioStation();
+        int expected = 16;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prevRadioStationWithoutConstructorParametersWhenTheValueAreMinRadioStationNumber() {
         Radio radio = new Radio();
         radio.setCurrentRadioStationNumber(0);
         radio.prevRadioStation();
@@ -44,7 +82,17 @@ public class RadioTest {
     }
 
     @Test
-    public void prevRadioStationWhenTheValueAre9() {
+    public void prevRadioStationWithConstructorParametersWhenTheValueAreMaxRadioStationNumber() {
+        Radio radio = new Radio(22);
+        radio.setCurrentRadioStationNumber(21);
+        radio.prevRadioStation();
+        int expected = 20;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prevRadioStationWithoutConstructorParametersWhenTheValueAreMaxRadioStationNumber() {
         Radio radio = new Radio();
         radio.setCurrentRadioStationNumber(9);
         radio.prevRadioStation();
@@ -54,18 +102,36 @@ public class RadioTest {
     }
 
     @Test
-    public void prevRadioStationWhenTheValuesAreGreaterThan0AndLessThan9() {
-        Radio radio = new Radio();
-        radio.setCurrentRadioStationNumber(4);
+    public void prevRadioStationWithConstructorParametersWhenTheValuesAreGreaterThanMinRadioStationNumberAndLessThanMaxRadioStationNumber() {
+        Radio radio = new Radio(15);
+        radio.setCurrentRadioStationNumber(8);
         radio.prevRadioStation();
-        int expected = 3;
+        int expected = 7;
         int actual = radio.getCurrentRadioStationNumber();
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void prevRadioStationWithoutConstructorParametersWhenTheValuesAreGreaterThanMinRadioStationNumberAndLessThanMaxRadioStationNumber() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStationNumber(6);
+        radio.prevRadioStation();
+        int expected = 5;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
-    public void setNewRadioStationWhenTheValueAre9() {
+    public void setNewRadioStationWithConstructorParametersWhenTheValueAreMaxRadioStationNumber() {
+        Radio radio = new Radio(64);
+        radio.setCurrentRadioStationNumber(63);
+        int expected = 63;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNewRadioStationWithoutConstructorParametersWhenTheValueAreMaxRadioStationNumber() {
         Radio radio = new Radio();
         radio.setCurrentRadioStationNumber(9);
         int expected = 9;
@@ -74,7 +140,15 @@ public class RadioTest {
     }
 
     @Test
-    public void setNewRadioStationWhenTheValueAre0() {
+    public void setNewRadioStationWithConstructorParametersWhenTheValueAreMinRadioStationNumber() {
+        Radio radio = new Radio(4);
+        radio.setCurrentRadioStationNumber(0);
+        int expected = 0;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setNewRadioStationWithoutConstructorParametersWhenTheValueAreMinRadioStationNumber() {
         Radio radio = new Radio();
         radio.setCurrentRadioStationNumber(0);
         int expected = 0;
@@ -83,34 +157,61 @@ public class RadioTest {
     }
 
     @Test
-    public void setNewRadioStationWhenTheValuesAreGreaterThan0AndLessThan9() {
-        Radio radio = new Radio();
-        radio.setCurrentRadioStationNumber(6);
-        int expected = 6;
+    public void setNewRadioStationWithConstructorParametersWhenTheValuesAreGreaterThanMinRadioStationNumberAndLessThanMaxRadioStationNumber() {
+        Radio radio = new Radio(40);
+        radio.setCurrentRadioStationNumber(15);
+        int expected = 15;
         int actual = radio.getCurrentRadioStationNumber();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void setNewRadioStationWhenTheValuesAreGreaterThan9() {
+    public void setNewRadioStationWithoutConstructorParametersWhenTheValuesAreGreaterThanMinRadioStationNumberAndLessThanMaxRadioStationNumber() {
         Radio radio = new Radio();
-        radio.setCurrentRadioStationNumber(16);
+        radio.setCurrentRadioStationNumber(8);
+        int expected = 8;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNewRadioStationWithConstructorParametersWhenTheValuesAreGreaterThanMaxRadioStationNumber() {
+        Radio radio = new Radio(17);
+        radio.setCurrentRadioStationNumber(19);
         int expected = 0;
         int actual = radio.getCurrentRadioStationNumber();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void setNewRadioStationWhenTheValuesAreLessThan0() {
+    public void setNewRadioStationWithoutConstructorParametersWhenTheValuesAreGreaterThanMaxRadioStationNumber() {
         Radio radio = new Radio();
-        radio.setCurrentRadioStationNumber(-16);
+        radio.setCurrentRadioStationNumber(13);
         int expected = 0;
         int actual = radio.getCurrentRadioStationNumber();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void increaseSoundVolumeWhenTheValueAre0() {
+    public void setNewRadioStationWithConstructorParametersWhenTheValuesAreLessThanMinRadioStationNumber() {
+        Radio radio = new Radio(12);
+        radio.setCurrentRadioStationNumber(-6);
+        int expected = 0;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNewRadioStationWithoutConstructorParametersWhenTheValuesAreLessThanMinRadioStationNumber() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStationNumber(-4);
+        int expected = 0;
+        int actual = radio.getCurrentRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void increaseSoundVolumeWhenTheValueAreMinSoundVolume() {
         Radio radio = new Radio();
         radio.setCurrentSoundVolume(0);
         radio.increaseSoundVolume();
@@ -120,28 +221,28 @@ public class RadioTest {
     }
 
     @Test
-    public void increaseSoundVolumeWhenTheValueAre10() {
+    public void increaseSoundVolumeWhenTheValueAreMaxSoundVolume() {
         Radio radio = new Radio();
-        radio.setCurrentSoundVolume(10);
+        radio.setCurrentSoundVolume(100);
         radio.increaseSoundVolume();
-        int expected = 10;
+        int expected = 100;
         int actual = radio.getCurrentSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void increaseSoundVolumeWhenTheValuesAreGreaterThan0AndLessThan10() {
+    public void increaseSoundVolumeWhenTheValuesAreGreaterThanMinSoundVolumeAndLessThanMaxSoundVolume() {
         Radio radio = new Radio();
-        radio.setCurrentSoundVolume(4);
+        radio.setCurrentSoundVolume(48);
         radio.increaseSoundVolume();
-        int expected = 5;
+        int expected = 49;
         int actual = radio.getCurrentSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
 
     @Test
-    public void decreaseSoundVolumeWhenTheValueAre0() {
+    public void decreaseSoundVolumeWhenTheValueAreMinSoundVolume() {
         Radio radio = new Radio();
         radio.setCurrentSoundVolume(0);
         radio.decreaseSoundVolume();
@@ -151,36 +252,36 @@ public class RadioTest {
     }
 
     @Test
-    public void decreaseSoundVolumeWhenTheValueAre10() {
+    public void decreaseSoundVolumeWhenTheValueAreMaxSoundVolume() {
         Radio radio = new Radio();
-        radio.setCurrentSoundVolume(10);
+        radio.setCurrentSoundVolume(100);
         radio.decreaseSoundVolume();
-        int expected = 9;
+        int expected = 99;
         int actual = radio.getCurrentSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void decreaseSoundVolumeWhenTheValuesAreGreaterThan0AndLessThan10() {
+    public void decreaseSoundVolumeWhenTheValuesAreGreaterThanMinSoundVolumeAndLessThanMaxSoundVolume() {
         Radio radio = new Radio();
-        radio.setCurrentSoundVolume(4);
+        radio.setCurrentSoundVolume(33);
         radio.decreaseSoundVolume();
-        int expected = 3;
+        int expected = 32;
         int actual = radio.getCurrentSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void setNewCurrentSoundVolumeWhenTheValueAre10() {
+    public void setNewCurrentSoundVolumeWhenTheValueAreMaxSoundVolume() {
         Radio radio = new Radio();
-        radio.setCurrentSoundVolume(10);
-        int expected = 10;
+        radio.setCurrentSoundVolume(100);
+        int expected = 100;
         int actual = radio.getCurrentSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void setNewCurrentSoundVolumeWhenTheValueAre0() {
+    public void setNewCurrentSoundVolumeWhenTheValueAreMinSoundVolume() {
         Radio radio = new Radio();
         radio.setCurrentSoundVolume(0);
         int expected = 0;
@@ -189,7 +290,7 @@ public class RadioTest {
     }
 
     @Test
-    public void setNewCurrentSoundVolumeWhenTheValuesAreGreaterThan0AndLessThan10() {
+    public void setNewCurrentSoundVolumeWhenTheValuesAreGreaterThanMinSoundVolumeAndLessThanMaxSoundVolume() {
         Radio radio = new Radio();
         radio.setCurrentSoundVolume(6);
         int expected = 6;
@@ -198,16 +299,16 @@ public class RadioTest {
     }
 
     @Test
-    public void setNewCurrentSoundVolumeWhenTheValueGreaterThan10() {
+    public void setNewCurrentSoundVolumeWhenTheValueGreaterThanMaxSoundVolume() {
         Radio radio = new Radio();
-        radio.setCurrentSoundVolume(12);
+        radio.setCurrentSoundVolume(302);
         int expected = 0;
         int actual = radio.getCurrentSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void setNewCurrentSoundVolumeWhenTheValuesAreLessThan0() {
+    public void setNewCurrentSoundVolumeWhenTheValuesAreLessThanMinSoundVolumeAnd() {
         Radio radio = new Radio();
         radio.setCurrentSoundVolume(-6);
         int expected = 0;
